531a2ed660f315bfb1f8f5c51355776fd62d90db
Tuana <tuanaaunall10@gmail.com>
2026-07-16 10:56:35 +0300

    feat: generator model mimarisi tanimlandi

--- gans.py
+++ gans.py
@@ -17,6 +17,7 @@ from tensorflow.keras.datasets import fashion_mnist
 
 BUFFER_SIZE = 60000 # veri seti boyutu
 BATCH_SIZE = 128 # batch boyutu
+NOISE_DIM = 100 # generatora verilecek gurultu vektorunun boyutu
 
 # veri seti yukle
 (train_images, _), (_, _) = fashion_mnist.load_data() # sadece goruntuleri al, etiketleri kullanma
@@ -25,7 +26,22 @@ train_images = (train_images - 127.5) / 127.5 # normalize et -1 ile 1 arasina
 train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE) # veri setini shuffle et ve batch'le
 
 # generator modeli tanimla: fake goruntuler uretecek
-
+def make_generator_model():
+    model = tf.keras.Sequential([
+        layers.Dense(7*7*256, use_bias=False, input_shape=(NOISE_DIM,)), # ilk tam bagli katman, gurultuyu ozellik haritasina cevirir
+        layers.BatchNormalization(), # egitim stabilitesini arttirir
+        layers.LeakyReLU(), # negatif girisleri yumusatir
+        
+        layers.Reshape((7, 7, 256)), # tek boyutlu vektoru 3D ye donustur
+        
+        layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding="same", use_bias=False),
+        layers.BatchNormalization(),
+        layers.LeakyReLU(),
+        
+        layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding="same", use_bias=False, activation="tanh")
+    ])
+    
+    return model
 
 # discriminator modeli tanimla: gercek ve fake goruntuler arasinda ayirim yapacak
 
